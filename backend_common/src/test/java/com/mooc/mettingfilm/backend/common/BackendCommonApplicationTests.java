1d4277a03bbf2713acf963c6fbeef33eb63279d6
wangjunwei <zhengeili@qq.com>
2020-04-16 16:11:45 +0800

    第一次提交

---
+++ backend_common/src/test/java/com/mooc/mettingfilm/backend/common/BackendCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooc.mettingfilm.backend.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
