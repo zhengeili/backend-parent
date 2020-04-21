3db0601102ea89fa6ed29d13bb96bd7cd4aee6fb
wangjunwei <zhengeili@qq.com>
2020-04-21 22:18:27 +0800

    4-8代码

---
+++ backend_show/backend_show_provider/src/main/java/com/mooc/meetingfilm/provider/controller/ProviderController.java
@@ -0,0 +1,18 @@
+package com.mooc.meetingfilm.provider.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ProviderController {
+
+    @Value("${server.port}")
+    private int port;
+
+    public String providerSayHello(String message){
+        log.error("provider sayhello port:{}, message:{}",port,message);
+        return "Provider sayhello port:"+port+" , message:"+message;
+    }
+}
