ee0eb4b3770abfacc9cf6ac1c37577763d7d8d28
wangjunwei <zhengeili@qq.com>
2020-04-21 12:20:05 +0800

    4-4eureka搭建

---
+++ backend_eureka_server/src/main/java/com/mooc/meetingfilm/eureka/BackendEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.mooc.meetingfilm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEurekaServerApplication.class, args);
+    }
+
+}
