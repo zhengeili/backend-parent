cfc7ddc93dc1cc22c902a8b8d58757cba694f918
wangjunwei <zhengeili@qq.com>
2020-04-24 23:45:11 +0800

    6-2代码

---
+++ backend_hall/src/main/java/com/mooc/meetingfilm/hall/dao/mapper/MoocFieldTMapper.java
@@ -0,0 +1,16 @@
+package com.mooc.meetingfilm.hall.dao.mapper;
+
+import com.mooc.meetingfilm.hall.dao.entity.MoocFieldT;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 放映场次表 Mapper 接口
+ * </p>
+ *
+ * @author wjw
+ * @since 2020-04-24
+ */
+public interface MoocFieldTMapper extends BaseMapper<MoocFieldT> {
+
+}
---
+++ backend_hall/src/test/java/com/mooc/meetingfilm/hall/BackendHallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooc.meetingfilm.hall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
