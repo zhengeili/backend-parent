b81c885fb299214a7c6ea15797519e1e4887c86f
wangjunwei <zhengeili@qq.com>
2020-04-29 09:06:27 +0800

    6-7代码

--- backend_hall/src/main/java/com/mooc/meetingfilm/hall/service/HallServiceImpl.java
+++ backend_hall/src/main/java/com/mooc/meetingfilm/hall/service/HallServiceImpl.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mooc.meetingfilm.hall.controller.vo.HallSavedReqVO;
 import com.mooc.meetingfilm.hall.controller.vo.HallsRepVO;
 import com.mooc.meetingfilm.hall.controller.vo.HallsRespVO;
+import com.mooc.meetingfilm.hall.dao.entity.MoocFieldT;
 import com.mooc.meetingfilm.hall.dao.entity.MoocHallFilmInfoT;
 import com.mooc.meetingfilm.hall.dao.mapper.MoocFieldTMapper;
 import com.mooc.meetingfilm.hall.dao.mapper.MoocHallFilmInfoTMapper;
@@ -14,7 +15,6 @@ import com.mooc.meetingfilm.utils.util.ToolUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import javax.management.Query;
 
 @Service
 public class HallServiceImpl implements HallServiceAPI{
@@ -44,11 +44,39 @@ public class HallServiceImpl implements HallServiceAPI{
 
     /**
      * 保存影厅信息
-     * @param hallSavedReqVO
+     * @param reqVO
      * @throws CommonServiceException
      */
     @Override
-    public void saveHall(HallSavedReqVO hallSavedReqVO) throws CommonServiceException {
+    public void saveHall(HallSavedReqVO reqVO) throws CommonServiceException {
+        //播放厅列表数据
+        MoocFieldT field=new MoocFieldT();
 
+        field.setCinemaId(ToolUtils.str2Int(reqVO.getCinemaId()));
+        field.setFilmId(ToolUtils.str2Int(reqVO.getFilmId()));
+        field.setBeginTime(reqVO.getBeginTime());
+        field.setEndTime(reqVO.getEndTime());
+        field.setHallId(ToolUtils.str2Int(reqVO.getHallTypeId()));
+        field.setHallName(reqVO.getHallName());
+        field.setPrice(ToolUtils.str2Int(reqVO.getFilmPrice()));
+
+        fieldTMapper.insert(field);
+
+        //播放厅对应的影片数据，影片荣誉数据，缓存有一份
+        MoocHallFilmInfoT hallFilmInfo=describeFilmInfo(reqVO.getFilmId());
+
+        hallFilmInfoTMapper.insert(hallFilmInfo);
+    }
+    private MoocHallFilmInfoT describeFilmInfo(String filmId) throws  CommonServiceException{
+        MoocHallFilmInfoT hallFilmInfo=new MoocHallFilmInfoT();
+        hallFilmInfo.setFilmId(0);
+        hallFilmInfo.setFilmName("");
+        hallFilmInfo.setFilmLength("");
+        hallFilmInfo.setFilmCats("");
+        //hallFilmInfo.setFilmLanguage("");//默认生成好的，这里不再赋值
+        hallFilmInfo.setActors("");
+        hallFilmInfo.setImgAddress("");
+
+        return  hallFilmInfo;
     }
 }
