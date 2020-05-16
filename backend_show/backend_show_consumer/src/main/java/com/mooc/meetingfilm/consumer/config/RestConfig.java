451e23bca8c526ff84b4343b7f2baee1061ecdb2
wangjunwei <zhengeili@qq.com>
2020-05-16 23:22:03 +0800

    “7-6代码1

--- backend_show/backend_show_consumer/src/main/java/com/mooc/meetingfilm/consumer/config/RestConfig.java
+++ backend_show/backend_show_consumer/src/main/java/com/mooc/meetingfilm/consumer/config/RestConfig.java
@@ -1,5 +1,8 @@
 package com.mooc.meetingfilm.consumer.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +14,10 @@ public class RestConfig {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule iRule(){
+        return new RoundRobinRule();
     }
 }
