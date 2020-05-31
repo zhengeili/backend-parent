61bd9dce267bd229cd011ff723b39ecbfbd3fff7
wangjunwei <zhengeili@qq.com>
2020-05-31 15:25:06 +0800

    9-6代码

--- backend_show/backend_show_consumer/src/main/java/com/mooc/meetingfilm/consumer/feign/ProviderApi.java
+++ backend_show/backend_show_consumer/src/main/java/com/mooc/meetingfilm/consumer/feign/ProviderApi.java
@@ -4,12 +4,12 @@ import com.mooc.meetingfilm.consumer.feign.vo.UserModel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "providerTest",url = "http://localhost:7101")
+@FeignClient(name = "providerTest",path = "/provider",url = "http://localhost:7101")
 public interface ProviderApi {
-    @RequestMapping(value = "/provider/sayhello", method = RequestMethod.GET)
+    @RequestMapping(value = "/sayhello", method = RequestMethod.GET)
     String invokerProviderController(@RequestParam("message") String message);
 
-    @RequestMapping(value = "/provider/{providerId}/sayhello", method = RequestMethod.POST)
+    @RequestMapping(value = "/{providerId}/sayhello", method = RequestMethod.POST)
     String providerPost(
             @RequestHeader("author") String author,
             @PathVariable("providerId") String providerId,
