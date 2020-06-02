60a9c99903a5b8550a9baa6f07d04137a6100ecd
wangjunwei <zhengeili@qq.com>
2020-06-02 09:29:55 +0800

    10-3代码

---
+++ backend_apigw_zuul/src/main/java/com/mooc/meetingfilm/backendapigwzuul/BackendApigwZuulApplication.java
@@ -0,0 +1,13 @@
+package com.mooc.meetingfilm.backendapigwzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApigwZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApigwZuulApplication.class, args);
+    }
+
+}
